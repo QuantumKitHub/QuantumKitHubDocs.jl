7deac0a19386819fc0fff65ba8fd274b370053ac
Documenter.jl <documenter@juliadocs.github.io>
2025-11-08 19:52:36 +0000

    build based on 1d4b6fa

---
+++ dev/TensorOperations/versions.js
@@ -0,0 +1,21 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v5.3",
+  "v5.2",
+  "v5.1",
+  "v5.0",
+  "v4.1",
+  "v4.0",
+  "v3.2",
+  "v3.1",
+  "v3.0",
+  "v2.2",
+  "v2.1",
+  "v2.0",
+  "v1.3",
+  "v1.1",
+  "v1.0",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "v5.3.0";
+var DOCUMENTER_STABLE = "stable";
